6725e1aaf0f70a2ff12b70b2e333b6d573b846d6
Kajetan Johannes Hammerle <kajetan@hammerle.me>
2021-05-11 23:11:48 +0200

    updated team

--- team.php
+++ team.php
@@ -1,12 +1,12 @@
 <?php
 
 $teamData = [
-    ["name" => "Kajetan", "age" => "24", "ingame" => "kajetanjohannes", "text" => "Developer, Founder, Builder"],
-    ["name" => "Marvin", "age" => "23", "ingame" => "marvinius", "text" => "Developer"],
-    ["name" => "Luca", "age" => "23", "ingame" => "eropster", "text" => "Builder"],
-    ["name" => "Fabio", "age" => "22", "ingame" => "wertibaldi", "text" => "Builder"],
-    ["name" => "Jan", "age" => "18", "ingame" => "SirTerence7", "text" => "Moderator, Builder"],
-    ["name" => "Lucas", "age" => "18", "ingame" => "MentorMentos", "text" => "Moderator"]
+    ["name" => "Kajetan", "ingame" => "kajetanjohannes", "text" => "Owner, Developer"],
+    ["name" => "Marvin", "ingame" => "marvinius", "text" => "Admin, Developer"],
+    ["name" => "Luca", "ingame" => "eropster", "text" => "Admin, Builder"],
+    ["name" => "Fabio", "ingame" => "wertibaldi", "text" => "Admin, Builder"],
+    ["name" => "Jan", "ingame" => "SirTerence7", "text" => "Moderator, Builder"],
+    ["name" => "Lucas", "ingame" => "MentorMentos", "text" => "Admin, Influencer"]
 ];
 
 include "team.view.php";
--- team.view.php
+++ team.view.php
@@ -5,7 +5,7 @@
         <?php foreach($teamData as $data) : ?>
             <div class="col-md-6 col-lg-4 text-center">
                 <img src="images/team/<?= $data["ingame"] ?>.png" width="200" height="270" alt="<?= $data["name"] ?>">
-                <h3><?= $data["name"] ?> (<?= $data["age"] ?>)</h3>
+                <h3><?= $data["name"] ?></h3>
                 <h6 class="text-muted"><?= $data["ingame"] ?></h6>
                 <p><?= $data["text"] ?></p>
             </div>
